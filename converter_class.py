c9b652afb2a0636ef2285697734fc50a721b04f5
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 11:20:25 -0500

    Prueba 1 pasada con exito.

--- converter_class.py
+++ converter_class.py
@@ -4,3 +4,7 @@ class Converter(object):
     def __init__(self):
         """Constructor for class Converter."""
         pass
+
+    def convert(self, roman):
+        """Convert roman numbers to decimal numbers."""
+        return 1
