68da5767f462f9f3f8659f929d7779dd1c936450
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 16:36:37 -0500

    Se cambia el tipo de dato de la lista de numerales romanos: Lista de tuplas -> Tupla de tuplas (Mayor eficiencia)

--- converter_class.py
+++ converter_class.py
@@ -4,7 +4,7 @@ class Converter(object):
     def __init__(self):
         """Constructor for class Converter."""
         self.bufferedRoman = str()
-        self.romanNumerals = [("CM", 900), ("M", 1000), ("CD", 400), ("D", 500), ("XC", 90), ("C", 100), ("XL", 40), ("L", 50), ("IX", 9), ("X", 10), ("IV", 4), ("V", 5), ("I", 1)]
+        self.romanNumerals = (("CM", 900), ("M", 1000), ("CD", 400), ("D", 500), ("XC", 90), ("C", 100), ("XL", 40), ("L", 50), ("IX", 9), ("X", 10), ("IV", 4), ("V", 5), ("I", 1))
 
     def convert(self, roman):
         """Convert roman numbers to decimal numbers."""
