e3e33493547d54e7b2a980211202cdc05f57d561
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 15:04:44 -0500

    Prueba 9 a 16: Obtener la conversion de los numeros 40, 50, 90, 100, 400, 500, 900 y 1000. Pruebas superadas con exito

--- converter_test.py
+++ converter_test.py
@@ -52,6 +52,46 @@ class ConversorTest(unittest.TestCase):
         self.decimal = self.converter.convert("X")
         self.assertEquals(self.decimal, 10)
 
+    def test_get_forty(self):
+        """Test case for number XL."""
+        self.decimal = self.converter.convert("XL")
+        self.assertEquals(self.decimal, 40)
+
+    def test_get_fifty(self):
+        """Test case for number L."""
+        self.decimal = self.converter.convert("L")
+        self.assertEquals(self.decimal, 50)
+
+    def test_get_ninety(self):
+        """Test case for number XC."""
+        self.decimal = self.converter.convert("XC")
+        self.assertEquals(self.decimal, 90)
+
+    def test_get_one_hundred(self):
+        """Test case for number C."""
+        self.decimal = self.converter.convert("C")
+        self.assertEquals(self.decimal, 100)
+
+    def test_get_four_hundred(self):
+        """Test case for number CD."""
+        self.decimal = self.converter.convert("CD")
+        self.assertEquals(self.decimal, 400)
+
+    def test_get_five_hundred(self):
+        """Test case for number D."""
+        self.decimal = self.converter.convert("D")
+        self.assertEquals(self.decimal, 500)
+
+    def test_get_nine_hundred(self):
+        """Test case for number CM."""
+        self.decimal = self.converter.convert("CM")
+        self.assertEquals(self.decimal, 900)
+
+    def test_get_one_thousand(self):
+        """Test case for number M."""
+        self.decimal = self.converter.convert("M")
+        self.assertEquals(self.decimal, 1000)
+
     def tearDown(self):
         """Destroy context."""
         del(self.converter)
