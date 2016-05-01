9ede47aff28bf2d0a6cea2bf98fe18726b3a62b6
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 11:56:28 -0500

    Refactorizacion 1: Inicializar las variables necesarias para cada prueba

--- converter_test.py
+++ converter_test.py
@@ -7,19 +7,25 @@ from converter_class import Converter
 class ConversorTest(unittest.TestCase):
     """Class ConversorTest."""
 
+    def setUp(self):
+        """Arrange context."""
+        self.converter = Converter()
+        self.decimal = 0
+
     def test_get_one(self):
         """Test case for number I."""
-        converter = Converter()
-        decimal = 0
-        decimal = converter.convert("I")
-        self.assertEquals(decimal, 1)
+        self.decimal = self.converter.convert("I")
+        self.assertEquals(self.decimal, 1)
 
     def test_get_two(self):
         """Test case for number II."""
-        converter = Converter()
-        decimal = 0
-        decimal = converter.convert("II")
-        self.assertEquals(decimal, 2)
+        self.decimal = self.converter.convert("II")
+        self.assertEquals(self.decimal, 2)
+
+    def tearDown(self):
+        """Destroy context."""
+        del(self.converter)
+        del(self.decimal)
 
 if __name__ == '__main__':
         unittest.main()
