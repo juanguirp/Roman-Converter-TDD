b2eba5fbeafaf470ffcb8e965cdf622dc58a6757
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 14:17:07 -0500

    Prueba 5 superada con exito

--- converter_class.py
+++ converter_class.py
@@ -1,16 +1,10 @@
-import collections
-
-
 class Converter(object):
     """Class Converter."""
 
     def __init__(self):
         """Constructor for class Converter."""
         self.bufferedRoman = str()
-        self.romanNumerals = {
-            "IV": 4,
-            "I": 1
-        }
+        self.romanNumerals = [("IV", 4), ("V", 5), ("I", 1)]
 
     def convert(self, roman):
         """Convert roman numbers to decimal numbers."""
@@ -27,7 +21,7 @@ class Converter(object):
         if not self.bufferedRoman:
             return 0
 
-        for numeral, value in sorted(self.romanNumerals.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+        for (numeral, value) in self.romanNumerals:
             if numeral in self.bufferedRoman:
                 self.bufferedRoman = self.bufferedRoman.replace(numeral, "", 1)
                 return value
