58a419ef62a2879b2a42f398aa388263e312a753
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 11:38:35 -0500

    Prueba 2: Convertir II a 2. Fallo 1: 1 es diferente de 2

--- converter_test.py
+++ converter_test.py
@@ -8,11 +8,18 @@ class ConversorTest(unittest.TestCase):
     """Class ConversorTest."""
 
     def test_get_one(self):
-        """Test case for numer I."""
+        """Test case for number I."""
         converter = Converter()
         decimal = 0
         decimal = converter.convert("I")
         self.assertEquals(decimal, 1)
+
+    def test_get_two(self):
+        """Test case for number II."""
+        converter = Converter()
+        decimal = 0
+        decimal = converter.convert("II")
+        self.assertEquals(decimal, 2)
 
 if __name__ == '__main__':
         unittest.main()
