bad0e542e5f1d0b98b12b385abad7646a4c3b9d2
Juangui Restrepo <juangui.rp@gmail.com>
2016-05-01 14:02:13 -0500

    Refactorizacion 2: Obtener los numeros del 1 al 3 sumando de uno en uno (I en I)

--- converter_class.py
+++ converter_class.py
@@ -1,15 +1,33 @@
+import collections
+
+
 class Converter(object):
     """Class Converter."""
 
     def __init__(self):
         """Constructor for class Converter."""
-        self.romans = {
-            "I": 1,
-            "II": 2,
-            "III": 3,
-            "IV": 4
+        self.bufferedRoman = str()
+        self.romanNumerals = {
+            "IV": 4,
+            "I": 1
         }
 
     def convert(self, roman):
         """Convert roman numbers to decimal numbers."""
-        return self.romans[roman]
+        self.bufferedRoman = roman
+        convertedNumber = 0
+        maximum = self.getMaximumDigit()
+        while maximum:
+            convertedNumber += maximum
+            maximum = self.getMaximumDigit()
+        return convertedNumber
+
+    def getMaximumDigit(self):
+        """Return maximum digit in a roman number."""
+        if not self.bufferedRoman:
+            return 0
+
+        for numeral, value in sorted(self.romanNumerals.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+            if numeral in self.bufferedRoman:
+                self.bufferedRoman = self.bufferedRoman.replace(numeral, "", 1)
+                return value
